280ec646e933996be5f8f52fa631f6634ce6998a
AndelsonOliveira1995 <166122434+AndelsonOliveira1995@users.noreply.github.com>
2024-08-20 09:48:54 -0300

    Atualização
    Construção do formulário

--- app01/resources/views/welcome.blade.php
+++ app01/resources/views/welcome.blade.php
@@ -7,10 +7,28 @@
     <title>Evolium</title>
     <link rel="stylesheet" href="/css/materialize.min.css">
     <link rel="stylesheet" href="/fonts/material-icons.css">
+    <link rel="stylesheet" href="/css/home.css">
 </head>
 <body>
 
-    <h1>Olá Mundo</h1>
+    <div class="row">
+        <div class="col s12 l4 m3 offset-l4 registro">
+            <div class="card">
+                <div class="card-action">
+                    <h5 class="text-center white-text">Registro de Números de Telefones</h5>
+                </div>
+                <div class="card-content">
+                    <form action="" method="post">
+                        @csrf
+                        <div class="input-field m3">
+                            <input type="text" name="nome" id="nome">
+                            <label for="nome" class="grey-text">Nome Completo</label>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <script src="/js/jquery-3.7.1.min.js"></script>
